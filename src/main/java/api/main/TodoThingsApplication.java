0a8f4b5878a2aed00a1e22ecf21d18e5ef85f1d8
caiolopexd <r9506505@gmail.com>
2025-10-11 23:48:33 -0300

    Feat: Initializing springboot and db connection

---
+++ src/main/java/api/main/TodoThingsApplication.java
@@ -0,0 +1,13 @@
+package api.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoThingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoThingsApplication.class, args);
+    }
+
+}
